53f7cec56b6b0936c0ebdfacbcef67e8b145d23a
Moneer <moneer.alromimah@gmail.com>
2023-08-03 17:29:34 -0600

    add auth & api slices to manage auth state

--- full-auth/redux/store.ts
+++ full-auth/redux/store.ts
@@ -1,8 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { apiSlice } from "./services/apiSlice";
+import authReducer from "./features/authSlice";
 
 export const makeStore = () =>
   configureStore({
-    reducer: {},
+    reducer: {
+      [apiSlice.reducerPath]: apiSlice.reducer,
+      auth: authReducer,
+    },
     devTools: process.env.NODE_ENV !== "production",
   });
 
