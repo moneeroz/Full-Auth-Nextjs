d3a0cb6ec94ee0797a3cb9bc77787c7b3e05c4a6
Moneer <moneer.alromimah@gmail.com>
2023-08-03 20:19:07 -0600

    add custom hook

---
+++ full-auth/hooks/useRegister.ts
@@ -0,0 +1,49 @@
+import { useRegisterMutation } from "@/redux/features/authApiSlice";
+import { useRouter } from "next/navigation";
+import { ChangeEvent, FormEvent, useState } from "react";
+import { toast } from "react-toastify";
+
+export default function useRegister() {
+  const router = useRouter();
+  const [register, { isLoading }] = useRegisterMutation();
+
+  const [formData, setFormData] = useState({
+    first_name: "",
+    last_name: "",
+    email: "",
+    password: "",
+    re_password: "",
+  });
+
+  const { first_name, last_name, email, password, re_password } = formData;
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    register({ first_name, last_name, email, password, re_password })
+      .unwrap()
+      .then(() => {
+        toast.success("Please check email to verify account");
+        router.push("/auth/login");
+      })
+      .catch(() => {
+        toast.error("Failed to register account");
+      });
+  };
+
+  return {
+    first_name,
+    last_name,
+    email,
+    password,
+    re_password,
+    isLoading,
+    handleChange,
+    handleSubmit,
+  };
+}
